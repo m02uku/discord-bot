4a17492fe76b94e1f21555bf62f5eae268a77297
m02uku <m02ukuzrzr@gmail.com>
2024-09-07 17:36:00 +0900

    add: rps, dice

--- app/main.py
+++ app/main.py
@@ -1,36 +1,55 @@
-import os
+import random
+from os import environ
 
 import discord
-import dotenv
+from discord.ext import commands
 from server import server_thread
 
-dotenv.load_dotenv()
-
-TOKEN = os.environ.get("TOKEN")
-if not TOKEN:
-    raise ValueError("TOKEN is not set in environment variables")
-
-intents = discord.Intents.default()
-intents.message_content = True
-intents.voice_states = True
-client = discord.Client(intents=intents)
+intents = discord.Intents.all()
+bot = commands.Bot(intents=intents, command_prefix="!")
+tree = bot.tree
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f"Logged in as {client.user} (ID: {client.user.id})")
-    print("------")
+    print(f"Logged in as {bot.user.name}.")
+    await tree.sync()
+    print("Synced slash commands.")
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+@tree.command(
+    name="rps",
+    description="グー、チョキ、パーを指定またはランダムに出力します",
+)
+async def rps(ctx: discord.Interaction, choice: int = None):
+    choices = ["グー", "チョキ", "パー"]
+    if choice is None:
+        result = random.choice(choices)
+    elif choice in [0, 1, 2]:
+        result = choices[choice]
+    else:
+        await ctx.response.send_message(
+            "無効な選択です。0, 1, 2のいずれかを指定してください。"
+        )
         return
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+    embed = discord.Embed(title=f"{ctx.user.name}: {result}")
+    await ctx.response.send_message(embed=embed)
 
 
-# Koyeb用 サーバー立ち上げ
+@tree.command(
+    name="dice",
+    description="指定された数の6面ダイスを振ります",
+)
+async def dice(ctx: discord.Interaction, count: int):
+    if count < 1:
+        await ctx.response.send_message("自然数を指定してください。")
+        return
+    results = [random.randint(1, 6) for _ in range(count)]
+    result_text = ", ".join(map(str, results))
+    embed = discord.Embed(title=f"{ctx.user.name}のダイス結果", description=result_text)
+    await ctx.response.send_message(embed=embed)
+
+
 server_thread()
-client.run(TOKEN)
+bot.run(environ["TOKEN"])
